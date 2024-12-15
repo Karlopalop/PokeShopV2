342a9ffb1ef06738c54de059d21230f14ce94939
Karlopalop <karlopalop@gmail.com>
2024-12-15 21:19:43 +0100

    aa

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+const express = require('express');
+const path = require('path');
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('mock-api-data.json');
-const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(router);
-server.listen(process.env.PORT || 3000, () => {
-  console.log('JSON Server is running');
+const app = express();
+const jsonServerRouter = jsonServer.create();
+const jsonServerMiddlewares = jsonServer.defaults();
+const jsonRouter = jsonServer.router('mock-api-data.json');
+
+jsonServerRouter.use(jsonServerMiddlewares);
+jsonServerRouter.use(jsonRouter);
+
+app.use(express.static('./dist/pokeshop'));
+app.use('/api', jsonServerRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/pokeshop/index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
